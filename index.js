2ccc60c3ef28dda759b8b9502938b478bc846247
Usman Ahmed Malik <usmanmalik.me@gmail.com>
2021-02-25 16:34:16 +0100

    adding missing files

---
+++ index.js
@@ -0,0 +1,25 @@
+const Mustache = require('mustache');
+const fs = require('fs');
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+let DATA = {
+  date: new Date().toLocaleDateString('en-GB', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'Europe/Amsterdam',
+  }),
+};
+
+function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+
+generateReadMe();
